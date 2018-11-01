907f006eb9549e7fab9195309dda5cd85fd024da
nsisic1 <nsisic1@etf.unsa.ba>
2018-11-01 14:18:58 +0100

    Dodani atributi i kosntruktor klasi MedunarodniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -2,8 +2,16 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class MedunarodniBroj extends TelefonskiBroj {
 
+    private String drzava;
+    private String broj;
+
+    MedunarodniBroj(String drzava, String broj) {
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
     public String ispisi() {
-        return "";
+        return drzava + broj;
     }
     public int hashCode() {
         return 2;
