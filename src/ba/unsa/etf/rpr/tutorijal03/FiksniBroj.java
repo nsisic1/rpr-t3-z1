5d5d74fe7d746354c53dfbd86d5d7fc529cc9926
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-01 02:30:28 +0100

    Napravljena klasa FiksniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -0,0 +1,5 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class FiksniBroj {
+    
+}
---
+++ src/ba/unsa/etf/rpr/tutorijal03/TelefonskiBroj.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+abstract class TelefonskiBroj {
+
+    public abstract String ispisi();
+    @Override
+    public abstract int hashCode();
+}
