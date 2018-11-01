cbfe9d03c3d2694f59947cac4185b38d2e8fb9f6
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-01 02:35:56 +0100

    Definisan enum Grad

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -2,13 +2,29 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 public class FiksniBroj extends TelefonskiBroj {
 
+    enum Grad {
 
-    // TODO: skeleton
-    
-    // private Grad grad;
+        TRAVNIK("030"), ORASJE("031"), ZENICA("032"), SARAJEVO("033"), LIVNO("034"), TUZLA("035"), MOSTAR("036"), BIHAC("037"),
+        GORAZDE("038"), SIROKI("039"),
+        BRCKO("049"),
+        MRKONJIC("050"), BANJA("051"), PRIJEDOR("052"), DOBOJ("053"), SAMAC("054"), BIJELJINA("055"), ZVORNIK("056"),
+        PALE("057"), FOCA("058"), TREBINJE("059");
+
+
+        final String pozivni;
+        private Grad(String pozivni) {
+            this.pozivni = "";
+        }
+        public String getPozivni() {
+            return pozivni;
+        }
+    }
+
+    private Grad grad;
     private String broj;
 
-
+    FiksniBroj(Grad grad, String broj) {
+    }
 
     public String ispisi() {
         return "";
