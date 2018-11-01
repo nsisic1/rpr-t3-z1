9b4fed0b0cb2d8b0e55d8ad8b6afc97f639fae43
nsisic1 <nsisic1@etf.unsa.ba>
2018-11-01 14:17:04 +0100

    Dodana klasa MedunarodniBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MedunarodniBroj extends TelefonskiBroj {
+
+    public String ispisi() {
+        return "";
+    }
+    public int hashCode() {
+        return 2;
+    };
+}
